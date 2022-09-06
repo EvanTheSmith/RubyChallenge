d0768356f2fabcaa7f3dc9776fe3a44fb2bb7b4b
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-09-05 23:34:52 -0500

    refactored comments

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,14 +1,14 @@
 class ChallengeController < ActionController::Base
     def index
-      # Grab Twitter JSON
+      # Grab tweets from Twitter
       response = HTTParty.get('http://takehome.io/twitter')
       tweets = JSON.parse(response.body)
 
-      # Grab Facebook JSON
+      # Grab statuses from Facebook
       response = HTTParty.get('http://takehome.io/facebook')
       statuses = JSON.parse(response.body)
 
-      # Grab Instagram JSON
+      # Grab photos from Instagram
       response = HTTParty.get('http://takehome.io/instagram')
       photos = JSON.parse(response.body)
 
