dad3236f8b43a40a5ac714c34ea83190d46a63b7
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-09-06 00:00:12 -0500

    implemented error-handling, fixed some typos

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -1,16 +1,16 @@
 class ChallengeController < ActionController::Base
     def index
       # Grab tweets from Twitter
-      response = HTTParty.get('http://takehome.io/twitter')
-      tweets = JSON.parse(response.body)
+      response = HTTParty.get('https://takehome.io/twitter')
+      response.content_type == 'application/json' ? tweets = JSON.parse(response.body) : tweets = { error: "tweets could not be loaded" }
 
       # Grab statuses from Facebook
-      response = HTTParty.get('http://takehome.io/facebook')
-      statuses = JSON.parse(response.body)
+      response = HTTParty.get('https://takehome.io/facebook')
+      response.content_type == 'application/json' ? statuses = JSON.parse(response.body) : statuses = { error: "statuses could not be loaded" }
 
       # Grab photos from Instagram
-      response = HTTParty.get('http://takehome.io/instagram')
-      photos = JSON.parse(response.body)
+      response = HTTParty.get('https://takehome.io/instagram')
+      response.content_type == 'application/json' ? photos = JSON.parse(response.body) : photos = { error: "photos could not be loaded" }
 
       end_result = { twitter: tweets, facebook: statuses, instagram: photos }
 
